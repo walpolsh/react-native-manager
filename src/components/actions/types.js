d09af7b0304963aaeee7875b1241d73f3f0f6da4
Paul Walsh <twentytwentyk@live.com>
2017-08-16 17:05:05 -0400

    onPassword and Email change wired up

---
+++ src/components/actions/types.js
@@ -0,0 +1,5 @@
+export const EMAIL_CHANGED = 'email_changed';
+
+//^^ Named export
+
+export const PASSWORD_CHANGED = 'password_changed';
--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,7 +1,24 @@
-const INITIAL_STATE = { email: '' };
+import {
+  EMAIL_CHANGED, PASSWORD_CHANGED,
+} from '../components/actions/types';
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+};
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case EMAIL_CHANGED:
+      return { ...state, email: action.payload };
+    case PASSWORD_CHANGED:
+      return { ...state, password: action.payload };
+
+      //make a new object {} (the curly braces after return...)
+      //take all of the properties in my existing state and throw them into that object^
+      //then define the property email, give it a value action.payload
+      //the new value for email will overwrite the old property in the original state.
+
     default:
       return state;
   }
